b644c537724fc4226a1a85fe580f13b8bb478b00
Marcelo <santosmarcelo1995@live.com>
2021-03-28 13:43:03 -0300

    Lifecycle hooks

---
+++ lifecycle_hooks/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Lifecycle hooks</h1>
+</template>
+
+<script>
+    import { onMounted } from 'vue'
+
+    // export default {
+    //     mounted () {
+    //         console.log('Mounted!')
+    //     }
+    // }
+
+    export default {
+        setup () {
+            onMounted(() => {
+                console.log('Mounted composition API')
+            })
+        }
+    }
+</script>
