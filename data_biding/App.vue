ab99dac4d2ce10fe507ad18575abf9e84c0dd493
Marcelo <santosmarcelo1995@live.com>
2021-03-28 11:32:40 -0300

    Data biding

---
+++ data_biding/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1 v-bind:style="{ textDecoration }">Hello {{ name }}</h1>
+        <input type="text" v-model="name">
+
+        <br>
+
+        <a :href="link">Treinamento VueJS</a>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            name: 'Marcelo',
+            link: 'https://treinamento.vuejsbrasil.org',
+            textDecoration: 'underline'
+        })
+    }
+</script>
